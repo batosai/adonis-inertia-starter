c18abed37252d42e02ae0a309ca70c838dfa7ddc
Alain TRAN <alain.tran@opsone.net>
2026-02-03 09:42:39 +0100

    refactor: update layout structure to use UHeader and UContainer components, removing old header markup

--- inertia/layouts/default.vue
+++ inertia/layouts/default.vue
@@ -23,44 +23,47 @@ watch(
 
 <template>
   <UApp>
-    <header>
-      <div>
-        <div>
-          <Link route="home">
-            <svg
-              width="66"
-              height="24"
-              viewBox="0 0 105 38"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M0 0h7.5v15H0ZM7.5 15h7.5v15H7.5ZM15 30h7.5v7.5H15ZM22.5 15h7.5v15H22.5ZM30 0h7.5v15H30ZM45 0h7.5v30h15v-30h7.5v37.5h-30v-37.5ZM82.5 37.5V0H105v7.5H90V15h15v7.5H90V30h15v7.5H82.5Z"
-                fill="currentColor"
-              />
-            </svg>
-          </Link>
-        </div>
-        <div>
-          <nav>
-            <template v-if="page.props.user">
-              <span>{{ page.props.user.fullName }}</span>
-              <Form route="session.destroy">
-                <button type="submit">Logout</button>
-              </Form>
-            </template>
-            <template v-else>
-              <Link route="new_account.create">Signup</Link>
-              <Link route="session.create">Login</Link>
-              <UButton color="neutral" variant="outline">NUXT UI BUTTON</UButton>
-            </template>
-          </nav>
-        </div>
-      </div>
-    </header>
+    <UHeader>
+      <template #title>
+        <Link route="home">
+          <svg
+            width="66"
+            height="24"
+            viewBox="0 0 105 38"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M0 0h7.5v15H0ZM7.5 15h7.5v15H7.5ZM15 30h7.5v7.5H15ZM22.5 15h7.5v15H22.5ZM30 0h7.5v15H30ZM45 0h7.5v30h15v-30h7.5v37.5h-30v-37.5ZM82.5 37.5V0H105v7.5H90V15h15v7.5H90V30h15v7.5H82.5Z"
+              fill="currentColor"
+            />
+          </svg>
+        </Link>
+      </template>
+      <template #right>
+        <nav class="flex items-center gap-4">
+          <template v-if="page.props.user">
+            <span>{{ page.props.user.fullName }}</span>
+            <Form route="session.destroy">
+              <button type="submit">Logout</button>
+            </Form>
+          </template>
+          <template v-else>
+            <Link route="new_account.create">
+              <ULink>Signup</ULink>
+            </Link>
+            <Link route="session.create">
+              <ULink>Login</ULink>
+            </Link>
+          </template>
+        </nav>
+      </template>
+    </UHeader>
   
-    <main>
-      <slot />
-    </main>
+    <UContainer>
+      <main>
+        <slot />
+      </main>
+    </UContainer>
   </UApp>
 </template>
