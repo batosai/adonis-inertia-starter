4a8244767168140e10f356296ebe866065a5c6f5
jeremy <jeremy.chaufourier@opsone.net>
2026-02-04 00:11:02 +0100

    feat: use uuid

--- app/models/user.ts
+++ app/models/user.ts
@@ -3,8 +3,9 @@ import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
 import { computed } from '@adonisjs/lucid/orm'
+import { UuidPrimaryKey } from '#models/mixins/uuid_primary_key'
 
-export default class User extends compose(UserSchema, withAuthFinder(hash)) {
+export default class User extends compose(UserSchema, UuidPrimaryKey, withAuthFinder(hash)) {
   @computed()
   get fullName() {
     return `${this.firstName} ${this.lastName}`
